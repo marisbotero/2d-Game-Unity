17e233b7de457ba13a70e7f0182bbf66b6aa38e0
marisbotero <maris2143@gmail.com>
2017-09-14 16:07:38 -0500

    PlayerRun , PlayerJump and EnemyRun

---
+++ primerjuego/Assets/New Folder/gamecontroler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamecontroler : MonoBehaviour {
+    
+    [Range (0f, 0.20f)]
+
+ 	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage platform;
+	public GameObject uiIdle;
+	public enum GameState{Idle, Playing};
+	public GameState gameState =GameState.Idle;
+
+	public GameObject Player;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//Empieza el juego
+		if (gameState == GameState.Idle && (Input.GetKeyDown ("up") || Input.GetMouseButtonDown(0))){
+			gameState = GameState.Playing;
+			uiIdle.SetActive(false);
+			Player.SendMessage("UpdateState","PlayerRun");
+
+		}
+
+		//Juego en marcha
+
+		else if(gameState == GameState.Playing){
+			Parallax();
+	
+		}
+		
+	}
+
+
+	void Parallax(){
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f );
+		platform.uvRect = new Rect (platform.uvRect.x + finalSpeed*4, 0f, 1f, 1f );
+
+
+	}
+}
---
+++ primerjuego/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up") || Input.GetMouseButtonDown(0)){
+			UpdateState("PlayerJump2");
+		}
+		
+	}
+
+	public void UpdateState (string state = null){
+		if (state != null){
+			animator.Play(state);
+		}
+
+	}
+}
